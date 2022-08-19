04ce3453951460452e9959bf9a53a5e9fef017dd
Umesh Sujakhu <sujakhu.umesh@gmail.com>
2022-08-19 14:19:37 +0545

    remove config

---
+++ config/default.example.ts
@@ -0,0 +1,10 @@
+export default {
+    port: 1337,
+    host: "localhost",
+    dbUri: "mongodb://localhost:27017/rest-api",
+    saltWorkFactor: 10,
+    accessTokenTtl: "15m",
+    refreshTokenTtl: "1y",
+    privateKey: "your_private_key",
+    publicKey: "your_public_key",
+}
